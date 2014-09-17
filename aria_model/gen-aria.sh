062aadb6e49bfd34f86d3e1318ece6425cc480a1
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-09-18 05:18:51 +0900

    rename sasabody to aria

---
+++ aria_model/gen-aria.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [ "$1" ] ;
+then
+    PROJECT_DIR=$1;
+else
+    PROJECT_DIR=`pwd`;
+fi;
+echo "[gen-aria-model]";
+echo " build in $PROJECT_DIR";
+
+cd $PROJECT_DIR;
+roseus "(progn (load \"gen-aria.l\") (gen-urdf) (exit 1))" ;
+rosrun collada_urdf_jsk_patch urdf_to_collada aria.urdf aria.dae ;
+rosrun euscollada collada2eus aria.dae  aria.yaml aria.l ;
+## roseus "(progn (load \"aria.l\") (objects (list (setq a (aria)))))";
