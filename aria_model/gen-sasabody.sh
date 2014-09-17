f13ab63bc120210bd2536ed55e1e64d305106998
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-09-18 05:12:42 +0900

    fix cmake files for catkin_make

--- aria_model/gen-sasabody.sh
+++ aria_model/gen-sasabody.sh
@@ -1,5 +1,16 @@
+#!/usr/bin/env sh
 
+if [ "$1" ] ;
+then
+    PROJECT_DIR=$1;
+else
+    PROJECT_DIR=`pwd`;
+fi;
+echo "[gen-aria-model]";
+echo " build in $PROJECT_DIR";
+
+cd $PROJECT_DIR;
 roseus "(progn (load \"gen-sasabody.l\") (gen-urdf) (exit 1))" ;
 rosrun collada_urdf_jsk_patch urdf_to_collada sasabody.urdf sasabody.dae ;
 rosrun euscollada collada2eus sasabody.dae  sasabody.yaml sasabody.l ;
-roseus "(progn (load \"sasabody.l\") (objects (list (setq a (sasabody)))))";
+## roseus "(progn (load \"sasabody.l\") (objects (list (setq a (sasabody)))))";
