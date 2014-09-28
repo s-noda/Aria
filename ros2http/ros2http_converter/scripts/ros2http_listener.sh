612292ba7125d26f2534322b7d0ba73b25bfacc1
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-09-28 22:54:20 +0900

    add currentor_socket.sh

--- ros2http/ros2http_converter/scripts/ros2http_listener.sh
+++ ros2http/ros2http_converter/scripts/ros2http_listener.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 ## `rospack find ros2http`/ros2http_converter/build/install/ros2http_converter/bin/ros2http_converter com.github.ros2http.ros2http_converter.HttpListener ;
-## `rospack find ros2http`/ros2http_converter/build/install/ros2http_converter/bin/ros2http_converter com.github.ros2http.ros2http_converter.SocketListener ;
-`rospack find ros2http`/ros2http_converter/build/install/ros2http_converter/bin/ros2http_converter com.github.ros2http.ros2http_converter.CurrentorSocketNode ;
+`rospack find ros2http`/ros2http_converter/build/install/ros2http_converter/bin/ros2http_converter com.github.ros2http.ros2http_converter.SocketListener ;
+## `rospack find ros2http`/ros2http_converter/build/install/ros2http_converter/bin/ros2http_converter com.github.ros2http.ros2http_converter.CurrentorSocketNode ;
